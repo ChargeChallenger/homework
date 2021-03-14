06819cced40263b6ec928777069d7fd893a2b551
ChargeChallenger <chargechallenger@users.noreply.github.com>
2021-03-14 12:46:17 +0700

    completed first homework

---
+++ homework1_2/homework1_2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace homework1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0, y = 0, counter = 1;
+            char temp;
+            while (counter <= 10)
+            {
+                Console.WriteLine("Введите направление движения (С, Ю, В, З): ");
+                temp = char.Parse(Console.ReadLine());
+                switch(temp)
+                {
+                    case 'С':
+                    case 'с':
+                        x += 1;
+                        Console.WriteLine($"Шаг {counter}: Вы сходили на Север.");
+                        counter += 1;
+                        break;
+                    case 'Ю':
+                    case 'ю':
+                        x -= 1;
+                        Console.WriteLine($"Шаг {counter}: Вы сходили на Юг.");
+                        counter += 1;
+                        break;
+                    case 'В':
+                    case 'в':
+                        y -= 1;
+                        Console.WriteLine($"Шаг {counter}: Вы сходили на Восток.");
+                        counter += 1;
+                        break;
+                    case 'З':
+                    case 'з':
+                        y += 1;
+                        Console.WriteLine($"Шаг {counter}: Вы сходили на Запад.");
+                        counter += 1;
+                        break;
+                    default:
+                        Console.WriteLine("Такого направления нет!");
+                        break;
+                }
+            }
+            double result = Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+            Console.WriteLine($"Вы прошли {result} метра/ов.");
+        }
+    }
+}
