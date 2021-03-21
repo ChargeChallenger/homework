05e388d6038f00877c9352f18cd461462e0821d7
ChargeChallenger <chargechallenger@users.noreply.github.com>
2021-03-21 17:48:46 +0700

    completed first task of second homework

---
+++ homework2/homework2_1/homework2_1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace homework2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размерность массива:");
+            int arraySize = int.Parse(Console.ReadLine());
+            int[] array = new int[arraySize];
+            for (int i = 0; i < arraySize; i++)
+            {
+                Console.WriteLine($"Введите {i+1} элемент массива:") ;
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            Array.Sort(array);
+            Console.WriteLine("Массив отсортирован по возрастанию");
+            int uniqueCounter = 1; //Предполагается, что в массиве уже есть какое-либо число
+            for (int i = 1; i < arraySize; i++)
+            {
+                if (array[i] != array[i-1])
+                {
+                    uniqueCounter++;
+                }
+            }
+            Console.WriteLine($"Уникальных чисел: {uniqueCounter}");
+        }
+    }
+}
