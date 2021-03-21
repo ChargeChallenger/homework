c8e9eab533df692c95d8ae72f76e5f45ceca2eb7
ChargeChallenger <chargechallenger@users.noreply.github.com>
2021-03-21 13:29:46 +0700

    completed first addirional tasks and transfering previous homework to new folder

---
+++ additional_tasks/additional1_1/additional1_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace additional1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите натуральное число:");
+            int number = int.Parse(Console.ReadLine());
+            if ((number - 4) % 4 == 0)
+            {
+                Console.WriteLine("Да");
+            }
+            else
+            {
+                Console.WriteLine("Нет");
+            };
+        }
+    }
+}
---
+++ additional_tasks/additional1_2/additional1_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace additional1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] ones = { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+            string[] tens = { "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
+            string[] hundreds = { "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" };
+            string[] thousands = { "", "M", "MM", "MMM", "MMMM" };
+
+            Console.WriteLine("Введите число (от 0 до 4000):");
+            int number = int.Parse(Console.ReadLine());
+
+            string result = thousands[number / 1000];
+            result += hundreds[number / 100 % 10];
+            result += tens[number / 10 % 10];
+            result += ones[number % 10];
+
+            Console.WriteLine(result);
+        }
+    }
+}
