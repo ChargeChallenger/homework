20208194a2f83c5a4c3fdf2f600a84d1d0c24218
ChargeChallenger <chargechallenger@users.noreply.github.com>
2021-03-23 10:50:51 +0700

    added second program

---
+++ homework2/homework2_2/homework2_2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace homework2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размерность массива:");
+            int arraySize = int.Parse(Console.ReadLine());
+            int[,] squareArray = new int[arraySize, arraySize];
+            for (int columnIteration = 0; columnIteration < arraySize; columnIteration++)
+            {
+                for (int rowIteration = 0; rowIteration < arraySize; rowIteration++)
+                {
+                    Console.WriteLine($"Впишите {columnIteration + 1},{rowIteration + 1} элемент массива:");
+                    squareArray[columnIteration, rowIteration] = int.Parse(Console.ReadLine());
+                }
+            }
+            for (int columnIteration = 0; columnIteration < arraySize; columnIteration++)
+            {
+                for (int rowIteration = 0; rowIteration < arraySize; rowIteration++)
+                {
+                    if (squareArray[columnIteration, rowIteration] != squareArray[rowIteration, columnIteration])
+                    {
+                        goto notSymmetry;
+                    }
+                }
+            }
+            goto Symmetry;
+
+        notSymmetry:
+            Console.WriteLine("Массив не симметричен");
+
+        Symmetry:
+            Console.WriteLine("Массив симметричен");
+        }
+    }
+}
